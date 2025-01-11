5f369ee957aa322df73a97ef5d7939df5a5e4a89
RickSihel <richard.sihel@gmail.com>
2025-01-11 15:15:36 +0200

    Tirar GoLand

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let linguagensDeProgramacao = ['javaScript', 'C', 'C++', 'Kotlin', 'Python'];
 linguagensDeProgramacao.push ('java');
 linguagensDeProgramacao.push ('Ruby');
-linguagensDeProgramacao.push ('GoLang');
 console.log(linguagensDeProgramacao);
 console.log(linguagensDeProgramacao[2]);
 responsiveVoice.speak(linguagensDeProgramacao, 'Brazilian Portuguese Female', {rate:1.2});
